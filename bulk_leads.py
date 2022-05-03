2ab9e71abf51f837e1042706d56ebbff4080de3b
tajerek <tajerek@gmail.com>
2022-05-03 03:32:38 +0100

    Add files via upload

--- bulk_leads.py
+++ bulk_leads.py
@@ -1,36 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 
 import streamlit as st
 import pandas as pd
 
-###################################
-from st_aggrid import AgGrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-from st_aggrid.shared import JsCode
-
-###################################
 
 from functionforDownloadButtons import download_button
 
 ###################################
 
 
-def _max_width_():
-    max_width_str = f"max-width: 1800px;"
-    st.markdown(
-        f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-        unsafe_allow_html=True,
-    )
+
 
 st.set_page_config(page_icon="✂️", page_title="Bulk Leads")
 
